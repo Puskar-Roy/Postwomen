d583e3a73edfae6c7910f9abca2cfa4ffb035c0e
Puskar-Roy <puskarroy600@gmail.com>
2024-05-24 00:56:06 +0530

    feat:fonts

--- apps/web/components/DomainSection.tsx
+++ apps/web/components/DomainSection.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Input } from "./ui/input";
-import { ModeToggle } from "./theme-button";
 import { Button } from "./ui/button";
 const DomainSection = () => {
   return (
@@ -8,7 +7,6 @@ const DomainSection = () => {
       <div className="flex justify-center items-center flex-row gap-4">
         <Input/>
         <Button className="bg-indigo-500 hover:bg-indigo-400 dark:text-white">Send</Button>
-        <ModeToggle />
       </div>
     </div>
   );
--- apps/web/components/Resizebg.tsx
+++ apps/web/components/Resizebg.tsx
@@ -6,6 +6,15 @@ import {
 import DomainSection from "./DomainSection";
 import { TabsDemo } from "./tabs";
 import { ModeToggle } from "./theme-button";
+import Image from "next/image";
+import logo from "./../public/logo-api2.png";
+import { Alegreya_SC } from "next/font/google";
+
+const inter = Alegreya_SC({
+  weight: "700",
+  style: "normal",
+  subsets: ["latin"],
+});
 
 export default function Resizebg() {
   return (
@@ -13,8 +22,17 @@ export default function Resizebg() {
       <ResizablePanel defaultSize={50}>
         <ResizablePanelGroup direction="vertical">
           <ResizablePanel defaultSize={70}>
-            <div className="flex  items-center flex-col justify-center p-6">
-              <h1 className="text-4xl">Post Women</h1>
+            <div className="flex  items-center flex-col justify-center gap-10 p-6">
+              <div className="flex justify-center items-center gap-2 flex-row">
+                <Image
+                  src={logo}
+                  height={80}
+                  width={80}
+                  className="rounded-2xl"
+                  alt="logo"
+                />
+                <h1 className={`${inter.className} text-4xl `}>Post Women</h1>
+              </div>
               <div>
                 <TabsDemo />
                 <ModeToggle />
