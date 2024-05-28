bf282084b02713c9d656f49002c77bc915b3dc1a
Puskar-Roy <puskarroy600@gmail.com>
2024-05-28 20:33:29 +0530

    feat : parameter forms

--- apps/web/components/body-fields.tsx
+++ apps/web/components/body-fields.tsx
@@ -3,20 +3,20 @@ import React from 'react'
 const Bodyfields = () => {
   return (
     <div className='mt-10'>
-      <div className='flex gap-3 bg-slate-800 justify-between text-slate-200 p-3 rounded-lg text-sm'>
+      <div className='flex gap-3 bg-slate-800 justify-between text-slate-200 px-3 mx-auto rounded-lg text-sm w-[60%]'>
             <div className='p-2 hover:bg-green-400 font-semibold rounded-lg hover:text-black'>
                 Parameter
             </div>
-            <div>
+            <div className='p-2 hover:bg-green-400 font-semibold rounded-lg hover:text-slate-800'>
                 Body
             </div>
-            <div>
+            <div className='p-2 hover:bg-green-400 font-semibold rounded-lg hover:text-slate-800'>
                 Autharization
             </div>
-            <div>
+            <div className='p-2 hover:bg-green-400 font-semibold rounded-lg hover:text-slate-800'>
                 Query
             </div>
-            <div>
+            <div className='p-2 hover:bg-green-400 font-semibold rounded-lg hover:text-slate-800'>
                 Tests
             </div>
       </div>
